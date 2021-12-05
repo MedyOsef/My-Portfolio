391c799f9f28d8a9ef63c5b74c76929a38e56636
MedyOsef <porgomedyfaycal@gmail.com>
2021-12-05 20:22:40 +0000

    Responsive initialisation

--- fullpage-script.js
+++ fullpage-script.js
@@ -1,4 +1,14 @@
 new fullpage('#fullpage', {
     autoScrolling: true,
-    scrollingSpeed: 1150,
+    scrollingSpeed: 950,
+    fitToSectionDelay: 300,
+    easing: 'easeInOutCubic',
+    css3: true,
+    easingcss3: 'ease-out',
+    loopBottom: false,
+    navigation: false,
+    menu: '#nav',
+    anchors: ['About', 'Inspirations', 'Projects', 'Contact'],
+    navigationTooltips: ['About me', 'Inspirations', 'Projects', 'Contact me'],
+    showActiveTooltip: false,
 });
